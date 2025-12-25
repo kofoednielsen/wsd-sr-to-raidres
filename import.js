7505549e6833649a1f686fdd1e0517de3956c845
Asbjørn Kofoed-Nielsen <asbjorn@kofoednielsen.com>
2025-12-25 18:39:05 +0100

    more better!

--- import.js
+++ import.js
@@ -1,12 +1,26 @@
+
 console.log("Content-script loaded")
-async function registerScript(message) {
+document.querySelector("html").innerHTML += '<button id="wsd-import" suppressHydrationWarning>WSD import</button>'
+
+document.querySelector("#wsd-import").onclick = async () => {
+  var wsd_sr_plus = JSON.parse(prompt("Input Watership Down SR+ data"))
+
+
+  var sr_data = await (await fetch("https://raidres.top/api/events/K27SZ5")).json()
+  var updates = []
+  for (const res of sr_data.reservations) {
+    for (const plus of wsd_sr_plus) {
+      if (plus.attendee == res.character.name && res.raidItemId == plus.item_id) {
+        updates.push({"reservationId": res.id, "isValid": true, "value": plus.sr_plus})
+      }
+    }
+  }
+
   console.log("Called")
   raid_id = window.location.href.split("/").pop()
-  r = await fetch("https://raidres.top/api/events/K27SZ5/sr-plus", {
+  fetch("https://raidres.top/api/events/K27SZ5/sr-plus", {
     "method": "PUT",
     "Origin": "https://raidres.fly.dev",
-    "body": "{\"reference\":\"K27SZ5\",\"reservations\":[{\"reservationId\":3660161,\"isValid\":false,\"value\":10}]}"
+    "body": JSON.stringify({"reference": raid_id, "reservations": updates})
   });
 }
-registerScript()
-
--- popup/import-action.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function import_action() {
-  browser.runtime.sendMessage("import");
-}
-
-document.querySelector("#import").addEventListener('click', import_action);
